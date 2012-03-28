4333ab75ea1c7c60073cee49b0686a5643cf0823
BobZhome <BobZhome@Cheerful.com>
2012-03-27 20:31:27 -0400

    update

--- thunderc_VS660.mk
+++ thunderc_VS660.mk
@@ -1,4 +1,5 @@
 # We're on Verizon (TODO)
+CDMA_BRAND := verizon
 CDMA_GOOGLE_BASE := android-verizon
 CDMA_CARRIER_ALPHA := Verizon_Wireless
 CDMA_CARRIER_NUMERIC := 310012
@@ -12,3 +13,9 @@ $(call inherit-product, device/lge/thunderc_common/thunderc_common.mk)
 PRODUCT_COPY_FILES += \
     vendor/lge/thunderc/proprietary/VS660/system/lib/egl/libGLES_android.so:system/lib/egl/libGLES_android.so \
     vendor/lge/thunderc/proprietary/VS660/system/lib/libloc_ext.so:system/lib/libloc_ext.so
+
+# Add in model specific kernel modules
+PRODUCT_COPY_FILES += \
+    device/lge/thunderc_VS660/files/kernel/wireless.ko:system/lib/modules/wireless.ko \
+    device/lge/thunderc_VS660/files/kernel/tun.ko:system/lib/modules/tun.ko \
+    device/lge/thunderc_VS660/files/kernel/cifs.ko:system/lib/modules/cifs.ko
